7ffd017f210fa9dedf512004813069cce8fb7cb2
Utkarsh Mudgal <Nothingislive@gmail.com>
2023-07-10 01:22:46 +0530

    Progress 9-july-2023

---
+++ src/StringMethods.java
@@ -0,0 +1,45 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class StringMethods {
+    public static void main(String[] args) {
+        String a = "Nishu";
+        String as =new String("Nishkarsh");
+//        System.out.println(a);
+//        int  p =a.length();
+//        String p =a.toUpperCase();
+//        System.out.println(p);
+//        String nonTrimmedString = "        NIshKarsh     ";
+//        System.out.println(nonTrimmedString.trim());
+//        System.out.println(a.substring(2));
+//        System.out.println(a.substring(1,5));
+//        System.out.println(a.replace('i','u'));
+//        System.out.println(a.startsWith("Ni"));
+//        System.out.println(a.endsWith("shu"));
+//        System.out.println(a.charAt(0));
+//        System.out.println(a.indexOf("hu"));
+//        System.out.println(a.indexOf("s",3));
+//        System.out.println(a.equals("Nishu"));
+//        System.out.println(a.equalsIgnoreCase("NisHU"));
+//        Practie Sheet #3
+//        #problem 1
+//        String text = "LowerCase";
+//        text = text.toLowerCase();
+//        System.out.println(text);
+//        #Problem 2
+//        String str =new String("Hari Ram");
+//        System.out.println(str.replace(" ","_"));
+//        Problem 3
+//        Scanner sc = new Scanner(System.in);
+//        String input = sc.nextLine();
+//        System.out.println("Dear," +input + " Thanks a lot");
+//         Problem 4
+//        String aloo = "chulu  bhar    paani peelo";
+//        System.out.println(aloo.indexOf("  "));
+//        System.out.println(aloo.indexOf("   "));
+        // Excercise 5
+        String letter = " \" Dear Harry , this java course is nice \" ";
+        System.out.println(letter);
+
+    }
+}
---
+++ src/logicalOperators.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+public class logicalOperators {
+    public static void main(String[] args) {
+         If Else Statments
+        System.out.println("Hey There,Kindly Enter your Higher Secondary Percentage");
+        Scanner sc = new Scanner(System.in);
+        float grade = sc.nextFloat();
+        if(grade>=75.0){
+            System.out.println("Yes , You can take admission in LIT");
+        }
+        else {
+            System.out.println("Bhag yahan se langoor!");
+        }
+         Logical Operators
+        boolean a = true;
+        boolean b = true;
+        boolean c =false;
+        if (a || b && c){
+            System.out.println("hurrey");
+        }else {
+            System.out.println("Buzzinga");
+        }
+        Else IF LADDER SYSTEMMM
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Your Annual Salary in lpa");
+        float a = sc.nextFloat();
+        if (a > 44){
+            System.out.println("Khel hai bhai ka");
+        } else if (a>34) {
+            System.out.println("Badiya kar rhe ho");
+        } else if (a>22) {
+            System.out.println("Good worker , brother");
+        } else if (a>12) {
+            System.out.println("chal bhai Zinda to hai tu");
+        }else {
+            System.out.println("rehende bhai theela khol lete hai");
+        }
+
+        // Switch Case & Enhanched Switch Case Syntaxxx
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Your Grade(A,B,C,else) to get your remark");
+        String a = sc.next();
+        switch (a) {
+            case "A" -> System.out.println("Out Standing");
+            case "B" -> System.out.println("Very Good");
+            case "C" -> System.out.println("Nice");
+            default -> System.out.println("Better Luck Next time!");
+        }
+
+    }
+}
